16157eb7a6280a0b48976342da06cb3233156062
Daniel Marraccini <dmarraccini@FRSN.UTN.EDU.AR>
2024-07-31 17:31:26 -0300

    Primer ejemplo en clases sobre POO

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿abstract class FormaGeometrica
+{
+
+    public abstract double CalcularArea();
+}
+
+class Rectangulo : FormaGeometrica
+{
+
+    public double _Largo
+    {
+        get; 
+        set;
+    }
+
+    public double _Ancho
+    {
+        get; 
+        set;
+    }
+
+    public Rectangulo(double largo, double ancho)
+    {
+
+        _Largo = largo;
+        _Ancho = ancho;
+    }
+
+    public override double CalcularArea()  //override sobreescribe el original.
+    {
+
+        return _Largo * _Ancho;
+    }
+}
+
+class Program
+{
+
+    static void Main()
+    {
+
+        FormaGeometrica rectangulo = new Rectangulo(4, 6);
+
+        Console.WriteLine("Area del rectangulo: " + rectangulo.CalcularArea());
+
+    }
+}
